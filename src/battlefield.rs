4ff7bf1b3bb4b9d45098e1e8bfb8d941397a51aa
succcubbus <jannis.kaiser2@gmail.com>
2018-11-14 11:19:07 +0100

    refactor into mvc

---
+++ src/battlefield.rs
@@ -0,0 +1,9 @@
+pub struct Battlefield {
+    pub rotation: f64,
+}
+
+impl Battlefield {
+    pub fn new() -> Battlefield {
+        Battlefield { rotation: 0.0 }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,49 +5,17 @@ extern crate piston;
 
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events, EventLoop};
+use piston::event_loop::{EventSettings, Events};
 use piston::input::*;
 use piston::window::WindowSettings;
 use std::f32::consts;
+pub use battlefield::Battlefield;
+pub use battlefield_controller::BattlefieldController;
+pub use battlefield_view::{BattlefieldView, BattlefieldViewSettings};
 
-pub struct App {
-    gl: GlGraphics,
-    rotation: f64,
-}
-
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
-        let (w, h) = (200.0, 200.0);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            use graphics::*;
-
-            clear([0.0; 4], gl);
-
-            let transform = c
-                .transform
-                .trans(x, y)
-                .rot_rad(rotation)
-                .trans(-w / 2.0, -h / 2.0);
-
-            let r = rotation as f32;
-            let color = [
-                r.sin(),
-                (r + consts::PI * 2.0 / 3.0).sin(),
-                (r + consts::PI * 4.0 / 3.0).sin(),
-                1.0,
-            ];
-            let shape = rectangle::rectangle_by_corners(0.0, 0.0, w, h);
-            rectangle(color, shape, transform, gl);
-        });
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        self.rotation += consts::PI as f64 * args.dt;
-    }
-}
+mod battlefield;
+mod battlefield_controller;
+mod battlefield_view;
 
 pub fn run() {
     let opengl = OpenGL::V3_3;
@@ -59,20 +27,24 @@ pub fn run() {
         .build()
         .unwrap();
 
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-    };
-
     let mut events = Events::new(EventSettings::new());
+    let mut gl = GlGraphics::new(opengl);
+
+    let battlefield = Battlefield::new();
+    let mut battlefield_controller = BattlefieldController::new(battlefield);
+    let battlefield_view_settings = BattlefieldViewSettings::new();
+    let battlefield_view = BattlefieldView::new(battlefield_view_settings);
 
     while let Some(event) = events.next(&mut window) {
-        if let Some(r) = event.render_args() {
-            app.render(&r);
-        }
+        battlefield_controller.event(&event);
 
-        if let Some(u) = event.update_args() {
-            app.update(&u);
+        if let Some(r) = event.render_args() {
+            gl.draw(r.viewport(), |c, g| {
+                use graphics::{clear};
+                clear([0.0; 4], g);
+
+                battlefield_view.draw(&battlefield_controller, &c, g);
+            });
         }
     }
 }
